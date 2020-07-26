f53fbb0648afec94efcaeba32e7453ed5cec171f
ahmed japnoo <japnoo@gmail.com>
2020-07-26 01:11:13 +0100

    Added games table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 
 from app import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -7,6 +8,17 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    games = db.relationship('Games', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username) 
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    platform = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Games {}>'.format(self.body)
