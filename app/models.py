797c980721f90ad1d8a2645b1ca51bca6ca06365
ahmed japnoo <japnoo@gmail.com>
2020-07-26 22:02:54 +0100

    Started working on registration system (almost functional) and reworked table structure for user and game tables

--- app/models.py
+++ app/models.py
@@ -1,24 +1,42 @@
 
 from app import db
+from app import login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    games = db.relationship('Games', backref='author', lazy='dynamic')
+    created_at = db.Column(db.DateTime, nullable=False,
+                        default=datetime.utcnow)
+    modified_at = db.Column(db.DateTime, nullable=False,
+                         default=datetime.utcnow, onupdate=datetime.utcnow)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    
     def __repr__(self):
         return '<User {}>'.format(self.username) 
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(140))
-    platform = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    api_id = db.Column(db.String(255), primary_key=True)
+    title = db.Column(db.String(255))
+    platform = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, nullable=False,
+                        default=datetime.utcnow)
+    modified_at = db.Column(db.DateTime, nullable=False,
+                         default=datetime.utcnow, onupdate=datetime.utcnow)
     def __repr__(self):
-        return '<Games {}>'.format(self.body)
+        return '<Games {}>'.format(self.title)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, jsonify, request, flash, redirect
-
+from flask import Flask, render_template, jsonify, request, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
 from app.forms import LoginForm
-
 from app.data import get_games
+from app.models import User
 from app import app
 
 
@@ -26,8 +26,19 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        flash("Succesfully logged in", "success" )
-        return redirect('/')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password', "danger")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template("login.html", form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
