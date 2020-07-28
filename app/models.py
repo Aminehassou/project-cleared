7c03b2e21476564e5f13e3b3af3b3fdcfef0ac0a
ahmed japnoo <japnoo@gmail.com>
2020-07-28 16:44:22 +0100

    Added fully functional registration system

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 
-from app import db
-from app import login
+from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, jsonify, request, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
-from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required, LoginManager
+from werkzeug.urls import url_parse
+from app.forms import LoginForm, RegistrationForm
 from app.data import get_games
 from app.models import User
-from app import app
+from app import app, db
 
 
 def filter_query(item_list, query):
@@ -24,21 +25,41 @@ def browse_games():
 def home():
     return render_template("index.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+        
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("You have successfully registered!", "success")
+        return redirect(url_for("login"))
+    return render_template("register.html", form=form)
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for("index"))
+        
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password', "danger")
             return redirect(url_for('login'))
+        
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
     return render_template("login.html", form=form)
-
+@login_required
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('home'))
